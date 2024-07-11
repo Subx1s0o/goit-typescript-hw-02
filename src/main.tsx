3e632ac36733f28546e7b98f3e35bd4b9d27ce97
Subx1s0o <nazarsabardin@gmail.com>
2024-07-11 21:27:25 +0300

    create vite project

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import "modern-normalize/modern-normalize.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "reset-css/reset.css";
+import App from "./App.tsx";
+import "./index.css";
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
