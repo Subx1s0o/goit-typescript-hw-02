d0f1bc7e434437c4c0891312f13ce717ebde8bcb
Subx1s0o <nazarsabardin@gmail.com>
2024-10-29 23:38:41 +0200

    push

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <p>hi</p>
-    </>
-  );
-}
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import "modern-normalize/modern-normalize.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "reset-css/reset.css";
-import App from "./App.tsx";
+import App from "./components/App/App";
 import "./index.css";
-ReactDOM.createRoot(document.getElementById("root")!).render(
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
